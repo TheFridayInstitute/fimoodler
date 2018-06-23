2a4bd7d1a9a6577871e2d2527d64cb8d33aff1f6
Jim Creager <jhcreage@ncsu.edu>
2018-06-23 14:09:57 -0400

    Add fetch_quiz_grades

---
+++ R/quizzes.R
@@ -0,0 +1,39 @@
+
+
+#' Fetch quiz grades
+#'
+#' Returns quiz grades and the maximum possible grade for the specified quiz and
+#' users. Missing grades are given a value of \code{NA}.
+#'
+#' @param quiz_cm_id A single integer corresponding to a quiz course module id
+#'   (see \code{list_cms}).
+#' @param user_ids A vector of integers corresponding to userids.
+#' @param ... Potential future args.
+#' @param con A database connection object, using the session connection by
+#'   default.
+#' @return A tibble with quiz grade information for each input userid.
+#' @seealso \code{\link{list_cms}}
+#' @export
+fetch_quiz_grades <- function(quiz_cm_id, user_ids, ...,
+                              con = get_session_con()) {
+  quiz_id <- dplyr::tbl(con, "mdl_course_modules") %>%
+    dplyr::filter(id == quiz_cm_id) %>%
+    dplyr::collect(n = Inf) %>%
+    magrittr::use_series(instance)
+  max_grade <- dplyr::tbl(con, "mdl_quiz") %>%
+    dplyr::filter(id == quiz_id) %>%
+    dplyr::select(maxgrade = grade) %>%
+    dplyr::collect(n = Inf) %>%
+    magrittr::use_series(maxgrade)
+  grades <- dplyr::tbl(con, "mdl_quiz_grades") %>%
+    dplyr::filter(quiz == quiz_id, userid %in% user_ids) %>%
+    dplyr::select(userid, grade, gradetm = timemodified) %>%
+    dplyr::collect(n = Inf)
+
+  # Result needs one row per input user
+  res <- dplyr::tibble(userid = user_ids) %>%
+    dplyr::left_join(grades, by = "userid") %>%
+    dplyr::mutate(maxgrade = max_grade) %>%
+    dplyr::select(-userid)
+  return(res)
+}
